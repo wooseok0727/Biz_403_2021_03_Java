abc1e025b5408ba7ccfcbf6111fef3f97b317d1c
wooseok0727 <crash2@naver.com>
2021-03-17 16:38:27 +0900

    변수

---
+++ Java_001_Value/src/com/callor/values/var/Variable_05.java
@@ -0,0 +1,20 @@
+package com.callor.values.var;
+
+public class Variable_05 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 0;
+		int num2 = 0;
+		int sum = 0;
+		
+		num1 = 33;
+		num2 = 40;
+		sum = num1 + num2;
+		
+		// 계산된 결과를 sum 변수에 담아두고
+		// 여러번 코드에서 값을 읽어 사용할수 있다
+		System.out.printf("%d + %d = %d\n", num1, num2, sum);
+		System.out.printf("%d 와 %d 를 덧셈한 결과 %d\n", num1, num2, sum);
+	}
+}
