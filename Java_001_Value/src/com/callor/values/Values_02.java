1eccf63774fc24ab428dd708e9489e9167489806
wooseok0727 <crash2@naver.com>
2021-03-17 11:51:44 +0900

    Values

---
+++ Java_001_Value/src/com/callor/values/Values_02.java
@@ -0,0 +1,35 @@
+package com.callor.values;
+
+public class Values_02 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("=================================");
+		
+		System.out.println("반지름이 23인 원의 둘레와 넓이");
+		System.out.println("원의 둘레 : " + (23 * 2 * 3.14) );
+		System.out.println("원의 넓이 : " + (23 * 23 * 3.14) );
+		
+		System.out.println("=================================");
+		
+		
+		
+		
+		
+		System.out.println("=================================");
+		
+		System.out.println("원의 둘레, 넓이");
+		
+		System.out.println("=================================");
+		
+		System.out.println("반지름이 23인 원");
+		System.out.print("둘레 : ");
+		System.out.println(23 * 2 * 3.14);
+		System.out.print("넓이 : ");
+		System.out.println(23 * 23 * 3.14);
+		
+		System.out.println("=================================");
+		
+	}
+
+}
