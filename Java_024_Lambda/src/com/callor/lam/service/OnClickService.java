5d56ef747e5eec42e1d0b6890ec179e9a72a8079
wooseok0727 <crash2@naver.com>
2021-07-26 17:39:33 +0900

    람다

---
+++ Java_024_Lambda/src/com/callor/lam/service/OnClickService.java
@@ -0,0 +1,6 @@
+package com.callor.lam.service;
+
+public interface OnClickService {
+
+	public void onClick(String msg);
+}
---
+++ Java_024_Lambda/src/com/callor/lam/service/impl/OnClickServiceImplV1.java
@@ -0,0 +1,13 @@
+package com.callor.lam.service.impl;
+
+import com.callor.lam.service.OnClickService;
+
+public class OnClickServiceImplV1 implements OnClickService {
+
+	@Override
+	public void onClick(String msg) {
+		System.out.println(msg);
+		
+	}
+
+}
